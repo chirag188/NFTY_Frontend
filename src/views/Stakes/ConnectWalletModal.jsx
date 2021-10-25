957effab65ae6282744d8d53d3330fd93f35e46f
VivekMistry-SoluLab <vivek.mistry@solulab.co>
2021-10-25 19:15:19 +0530

    [Add]: added loader while connecting to wallet

--- src/views/Stakes/ConnectWalletModal.jsx
+++ src/views/Stakes/ConnectWalletModal.jsx
@@ -13,12 +13,14 @@ import {
   walletLink,
   fortmatic,
 } from "./../../utils/connectors";
+import { Spinner } from "react-bootstrap";
 
 const ConnectWalletModal = (props) => {
   const closeModal = () => {
     const { modalOpenClose } = props;
     modalOpenClose(false);
   };
+  const [loader, setLoader] = useState(false);
 
   const { account, activate, connector } = useWeb3React();
   const onboarding = useRef();
@@ -47,22 +49,26 @@ const ConnectWalletModal = (props) => {
   useEffect(() => {
     if (activatingConnector && activatingConnector === connector) {
       setActivatingConnector(undefined);
+      setLoader(false);
     }
   }, [activatingConnector, connector]);
 
   const onConnectWithWalletConnectClick = () => {
+    setLoader(true);
     setActivatingConnector(walletconnect);
     activate(walletconnect);
     localStorage.setItem("fortmaticConnect", "false");
   };
 
   const onLinkConnectClick = () => {
+    setLoader(true);
     setActivatingConnector(walletLink);
     activate(walletLink);
     localStorage.setItem("fortmaticConnect", "false");
   };
 
   const onConnectWithFortmaticClick = async () => {
+    setLoader(true);
     setActivatingConnector(fortmatic);
     activate(fortmatic);
     localStorage.setItem("fortmaticConnect", "true");
@@ -75,45 +81,53 @@ const ConnectWalletModal = (props) => {
       headerSubTitle="Choose your wallet and get ready to begin your NFTY journey."
       footerModalClass="p-0"
     >
-      <div className="connect-wallet-modal">
-        <div className="row text-center">
-          <div className="col-sm-6 d-flex justify-content-center">
-            <button className="btn" onClick={onConnectWithMetamaskClick}>
-              <div className="w-100 text-center">
-                <img src={metamaskImg} alt="" />
-              </div>
-              <span className="mt-2">Meta Mask</span>
-            </button>
+      {loader ? (
+        <Spinner
+          animation="border"
+          role="status"
+          style={{ alignSelf: "center", marginTop: "10%", marginBottom: "10%" }}
+        />
+      ) : (
+        <div className="connect-wallet-modal">
+          <div className="row text-center">
+            <div className="col-sm-6 d-flex justify-content-center">
+              <button className="btn" onClick={onConnectWithMetamaskClick}>
+                <div className="w-100 text-center">
+                  <img src={metamaskImg} alt="" />
+                </div>
+                <span className="mt-2">Meta Mask</span>
+              </button>
+            </div>
+            <div className="col-sm-6 d-flex justify-content-center">
+              <button className="btn" onClick={onConnectWithWalletConnectClick}>
+                <div className="w-100 text-center">
+                  <img className="mb-2" src={walletConnectImg} alt="" />
+                </div>
+                <span className="">Wallet Connect</span>
+              </button>
+            </div>
           </div>
-          <div className="col-sm-6 d-flex justify-content-center">
-            <button className="btn" onClick={onConnectWithWalletConnectClick}>
-              <div className="w-100 text-center">
-                <img className="mb-2" src={walletConnectImg} alt="" />
-              </div>
-              <span className="">Wallet Connect</span>
-            </button>
-          </div>
-        </div>
-        <div className="row text-center   ">
-          <div className="col-sm-6 d-flex justify-content-center">
-            <button className="btn" onClick={onConnectWithFortmaticClick}>
-              <div className="w-100 text-center">
-                <img className="" src={formaticeWallet} alt="" />
-              </div>
-              <span className="mt-2">Fortmatic</span>
-            </button>
-          </div>
+          <div className="row text-center   ">
+            <div className="col-sm-6 d-flex justify-content-center">
+              <button className="btn" onClick={onConnectWithFortmaticClick}>
+                <div className="w-100 text-center">
+                  <img className="" src={formaticeWallet} alt="" />
+                </div>
+                <span className="mt-2">Fortmatic</span>
+              </button>
+            </div>
 
-          <div className="col-sm-6 d-flex justify-content-center">
-            <button className="btn" onClick={onLinkConnectClick}>
-              <div className="w-100 text-center">
-                <img className="" src={coinbaseImg} alt="" />
-              </div>
-              <span className="mt-2">Coin Base</span>
-            </button>
+            <div className="col-sm-6 d-flex justify-content-center">
+              <button className="btn" onClick={onLinkConnectClick}>
+                <div className="w-100 text-center">
+                  <img className="" src={coinbaseImg} alt="" />
+                </div>
+                <span className="mt-2">Coin Base</span>
+              </button>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </Modal>
   );
 };
